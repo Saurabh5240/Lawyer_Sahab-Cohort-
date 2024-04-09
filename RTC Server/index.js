1f5efa4f416b825740110f288dad1ea7d226b4f3
SAURABH SANJEEV <129985013+Saurabh5240@users.noreply.github.com>
2024-04-09 13:59:00 +0530

    Add files via upload

---
+++ RTC Server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const CHAT_ENGINE_PROJECT_ID = "0f6661f4-5531-40a1-a467-a1bfa0d5dabd";
+const CHAT_ENGINE_PRIVATE_KEY = "0a6f4215-d972-42ec-b0fe-e5f2b62316e4";
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username:username,secret: username,first_name:username },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+
+// vvv On port 3001!
+app.listen(3001);
